2959352ed3acb210961ea664a350f6c0b7f248ae
Lyne <patriciamagne02@gmail.com>
2026-03-03 11:20:02 -0500

    annexe 7

--- Annexe7/app/src/main/java/com/lyne/annexe7/MainActivity.java
+++ Annexe7/app/src/main/java/com/lyne/annexe7/MainActivity.java
@@ -1,8 +1,14 @@
 package com.lyne.annexe7;
 
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_MOVE;
+import static android.view.MotionEvent.ACTION_UP;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     SurfaceDessin surf;
     ConstraintLayout main;
     Ecouteur ec;
+
+    Point depart, arrivee;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +39,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
         main = findViewById(R.id.main);
         surf = new SurfaceDessin(this);
         surf.setLayoutParams(new ConstraintLayout.LayoutParams(-1, -1));
         surf.setBackgroundResource(R.drawable.carte);
 
+        main.addView(surf);
+
+        //gestion d'évènement
+        //étape 1
         ec = new Ecouteur();
 
-        main.addView(surf);
+        //etape 2
+        surf.setOnTouchListener(ec);
+
 
     }
     private class SurfaceDessin extends View{
@@ -45,12 +62,21 @@ public class MainActivity extends AppCompatActivity {
         public SurfaceDessin(Context context) {
             super(context);
             crayon = new Paint(Paint.ANTI_ALIAS_FLAG);
+            crayon.setColor(Color.RED);
+            crayon.setStrokeWidth(15);
         }
 
         @Override
         protected void onDraw(@NonNull Canvas canvas) {
             super.onDraw(canvas);
 
+            if(depart != null){
+                canvas.drawRect(depart.x-25,depart.y-25,depart.x+25, depart.y+25, crayon);
+            }
+            if(depart != null && arrivee != null){
+                canvas.drawLine(depart.x,depart.y,arrivee.x,arrivee.y,crayon);
+                canvas.drawRect(arrivee.x-25,arrivee.y-25,arrivee.x+25, arrivee.y+25, crayon);
+            }
 
         }
     }
@@ -58,8 +84,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onTouch(View source, MotionEvent event) {
-            surf.invalidate();
-            return false;
+
+            if(event.getAction() == ACTION_DOWN){
+                depart = new Point((int)event.getX(), (int)event.getY());
+                surf.invalidate();
+            }else if(event.getAction() == ACTION_MOVE){
+                arrivee = new Point((int)event.getX(), (int)event.getY());
+                surf.invalidate();
+            }else if(event.getAction() == ACTION_UP){
+                surf.invalidate();
+            }
+
+            return true;
         }
     }
 }
