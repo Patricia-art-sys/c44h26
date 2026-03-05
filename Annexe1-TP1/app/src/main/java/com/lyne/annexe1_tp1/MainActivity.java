1642d3540fb7ff35528885142e14160118741eec
Lyne <patriciamagne02@gmail.com>
2026-03-05 10:48:36 -0500

    annexe-tp1

--- Annexe1-TP1/app/src/main/java/com/lyne/annexe1_tp1/MainActivity.java
+++ Annexe1-TP1/app/src/main/java/com/lyne/annexe1_tp1/MainActivity.java
@@ -3,9 +3,14 @@ package com.lyne.annexe1_tp1;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,7 +23,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     ConstraintLayout parent;
     Button onEnter;
+    TextView texteX, texteY;
     Surface s;
+    Ecouteur ec;
+    Path chemin;
+    int x, y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +42,50 @@ public class MainActivity extends AppCompatActivity {
 
         parent = findViewById(R.id.parent);
         onEnter = findViewById(R.id.button);
+        texteX = findViewById(R.id.texteX);
+        texteY = findViewById(R.id.texteY);
 
         s = new Surface(this);
         s.setLayoutParams(new ConstraintLayout.LayoutParams(-1,-1));
         parent.addView(s);
 
+        ec = new Ecouteur();
+        onEnter.setOnClickListener(ec);
+        chemin = new Path();
+    }
+    private class Ecouteur implements  View.OnClickListener{
+
+        @Override
+        public void onClick(View source) {
+            x = Integer.parseInt(texteX.getText().toString());
+            y = Integer.parseInt(texteY.getText().toString());
+
+            if(chemin.isEmpty()){
+                chemin.moveTo(x,y);
+            }else
+                chemin.lineTo(x, y);
+
+            s.invalidate(); //redessiner...
+        }
     }
     private class Surface extends View {
+        Paint crayon;
 
         public Surface(Context context){
             super(context);
             setBackgroundColor(Color.MAGENTA);
+            crayon = new Paint(Paint.ANTI_ALIAS_FLAG);
+            crayon.setStyle(Paint.Style.STROKE);
+            crayon.setStrokeWidth(10);
+            crayon.setColor(Color.BLACK);
         }
 
         @Override
         protected void onDraw(@NonNull Canvas canvas) {
             super.onDraw(canvas);
-            setBackgroundColor(Color.MAGENTA);
+            canvas.drawPath(chemin, crayon);
+
         }
     }
+
 }
